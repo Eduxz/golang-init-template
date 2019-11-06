7663ff484a1578aa84f415f5974ea1650b0c2475
Eduardo <eduardo.marquez@rappi.com>
2019-11-06 09:43:27 -0500

    initial commit

---
+++ conections/posgrest.go
@@ -0,0 +1,31 @@
+package pgconex
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+const (
+	host     = "localhost"
+	port     = 5400
+	user     = "eduxz"
+	dbname   = "system-manager"
+	password = "123456"
+)
+
+// Connect with Posgrest
+func Connect(callback func(database *gorm.DB)) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	db.LogMode(true)
+	callback(db)
+
+}
--- main.go
+++ main.go
@@ -25,5 +25,15 @@ func main() {
 	e.Static("/", "public")
 	e.GET("/something", getSomething)
 
+	// Restricted group
+	r := e.Group("/restricted")
+
+	// Configure middleware with the custom claims type
+	// config := middleware.JWTConfig{
+	// 	Claims:     &jwtCustomClaims{},
+	// 	SigningKey: []byte("secret"),
+	// }
+	r.Use(middleware.JWTWithConfig(config))
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
